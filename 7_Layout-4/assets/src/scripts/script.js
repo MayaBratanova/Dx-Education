3e4f2a23741c1a76bd5fc1b5bec35f670c8b0e66
mboyadzhieva <mboyadzhieva@devrix.com>
2019-07-29 18:07:45 +0300

    Fixing animations and side-aside place

--- 7_Layout-4/assets/src/scripts/script.js
+++ 7_Layout-4/assets/src/scripts/script.js
@@ -1,70 +1,50 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-	document.getElementsByTagName('body')[0].classList.add('loaded');
+    document.getElementsByTagName('body')[0].classList.add('loaded');   
 });
 
-$(function () {
-    addDelayClass();
-    getTopOffset();    
+$(function () {       
+    isOnScreen();
 })
 
-function addDelayClass () {
-    // let timeout = 1000;
-    // $(".dx-grossary").find('.dx-grossary-section').each(function () {
-    //     let $this = $(this);
-    //     setTimeout(function() {
-    //         $this.addClass("list-delay")
-    //     }, timeout); 
-    //     timeout += 800;
-    // })
 
-    
-    // let num = 0;
-
-    
-    
-    
-    
-        let timeout = 1000;
-        let list = $(".dx-grossary").find('.show');  
-        for (let i = 0; i < list.length; i++) {
-            let element = list[i];        
-            setTimeout(function() {
-                $(element).addClass("list-delay")
-            }, timeout); 
-            timeout += 800;
-        }     
-        // num = num+3;
-        // console.log(num);
-        // return num;
-        
-   
-  
-
-    // $(".dx-grossary").find('.show').each(function () {
-    //     let $this = $(this);
-    //     setTimeout(function() {
-    //         $this.addClass("list-delay")
-    //     }, timeout); 
-    //     timeout += 800;
-    // })
-}
-
-function getTopOffset () {
-    let element = $('.dx-grossary-a');    
-    let containerTopPoint = element.offset().top;
-    let containerHeight = element.height();    
-    let scrollHeight = containerHeight+containerTopPoint;
-}
-
-
-window.onscroll = function() {scrollScreen()};
-
-function scrollScreen() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        $('.hide').removeClass("hide").addClass("show");
-        addDelayClass();     
-
-    } else {
-        // $('.hide').removeClass("hide");
+function isOnScreen(elem) {
+	// if the element doesn't exist, abort
+	if( elem == undefined ) {
+		return;
     }
-}   
+
+	var $window = jQuery(window)
+	var viewport_top = $window.scrollTop()
+	var viewport_height = $window.height()
+	var viewport_bottom = viewport_top + viewport_height
+    var $elem = jQuery(elem)    
+	var top = $elem.offset().top
+	var height = $elem.height()
+    var bottom = top + height
+   
+
+	return (top >= viewport_top && top < viewport_bottom) ||
+	(bottom > viewport_top && bottom <= viewport_bottom) ||
+	(height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+}
+
+jQuery( document ).ready( function() {
+	window.addEventListener('scroll', function(e) {
+        $('.dx-grossary-section').each(function() {            
+            let $this = $(this);   
+          
+            if( isOnScreen($($this)) ) {
+                $this.addClass('visible')
+            }
+        })
+
+		
+    });
+    
+    window.addEventListener('scroll', function(e) {         
+        if( isOnScreen($('.dx-grossary-alphabet'))) {
+            $('.dx-grossary-alphabet').addClass('visible')
+        }
+    })
+});
+
