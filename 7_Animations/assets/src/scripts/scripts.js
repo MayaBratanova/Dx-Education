d8d7fe1c7127ae7a3e64c34af5253daf2dfd6864
mboyadzhieva <mboyadzhieva@devrix.com>
2019-09-02 17:24:22 +0300

    Changed the markup and the styles

--- 7_Animations/assets/src/scripts/scripts.js
+++ 7_Animations/assets/src/scripts/scripts.js
@@ -1,77 +1,53 @@
-jQuery( document ).ready( function ( $ ) {
-	'use strict';
-	/*
-	|--------------------------------------------------------------------------
-	| Developer mode
-	|--------------------------------------------------------------------------
-	|
-	| Set to true - it will allow printing in the console. Alsways check for this
-	| variables when running tests so you dont forget about certain console.logs.
-	| Id needed for development testing this variable should be used.
-	|
-	*/
-
-	const devMode = function() {
-		return true;
-	};
-
-	// Disable console.log for production site.
-	if ( ! devMode() ) {
-		console.log = function() {};
-	}
-	
-	//adding font-awesome icon 
-	$( "#search-2 form label" ).append( '<span class="search-icon"><i class="far fa-search" ></i></span>' );
-	
-	$( ".widget.widget_recent_entries ul li" ).prepend( '<span class="search-icon"><i class="far fa-caret-right" ></i></span>' );
-	
-	//search field widget
-	$( "#search-2 span.search-icon" ).click( function () {
-		$( "#search-2 span.search-icon" ).css({"transform": "translateX(100px)", "display": "none","cursor": "text"});
-		$( "#search-2 .search-field" ).addClass('search-spread');
-	} );
-	
-	//clarity search field 
-	$('.clarity-search-icon i').click(function(){
-		$(this).toggleClass('fa-times fa-search');
-		$(this).parents('.search-box').toggleClass('is-extended');		 
-	});
-	
-	//back to top animation 
-	$('.to-top-link').click(function(e) {
-		  e.preventDefault();
-			// When arrow is clicked
-		$('body,html').animate({
-			scrollTop : 0   // Scroll to top of body
-		}, 800);
-	});
-	
-	//changing the nagigation texts 
-	$(function(){
-		$('.navigation.post-navigation .nav-links .nav-previous > a').text('Older Posts');
-		$('.navigation.post-navigation .nav-links .nav-next > a').text('Newer Posts');
-	})
-	
-	//overrite textarea default row 
-	$(function(){
-		$('textarea').attr('rows', 4);
-	})
-	
-	
-	//comment nav text change 
-	$(function(){
-		$('#comment-nav-below .nav-links .nav-previous > a').text('Prev comment');
-		$('#comment-nav-below .nav-links .nav-next > a').text('Next comment');
-		
-		if($('#comment-nav-below .nav-links > div ').length < 2) {
-			$('#comment-nav-below .nav-links > div').css({'width':'100%','text-align':'right'});
-		}
-	})
-
-	//add focus effect
-	$('.site-header').find('.clarity-search-icon').on("click", function(e) {
-		e.preventDefault();
-		$('.site-header').find('.clarity-search').focus();
-		return false;
-	 });
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementsByTagName('body')[0].classList.add('is-loaded');   
 });
+
+$(function () {       
+    isOnScreen();
+})
+
+
+function isOnScreen(elem) {
+	// if the element doesn't exist, abort
+	if( elem == undefined ) {
+		return;
+    }
+
+	let $window = jQuery(window)
+	let viewport_top = $window.scrollTop()
+	let viewport_height = $window.height()
+	let viewport_bottom = viewport_top + viewport_height
+    let $elem = jQuery(elem)    
+	let top = $elem.offset().top
+	let height = $elem.height()
+    let bottom = top + height
+   
+
+	return (top >= viewport_top && top < viewport_bottom) ||
+	(bottom > viewport_top && bottom <= viewport_bottom) ||
+	(height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+}
+
+
+// Load the containers with the main content on the page after scrolling
+jQuery( document ).ready( function() {
+	window.addEventListener('scroll', function(e) {
+        $('.grossary-section').each(function() {            
+            let $this = $(this);   
+          
+            if( isOnScreen($($this)) ) {
+                $this.addClass('is-visible')
+            }
+        })
+
+		
+    });
+    
+    // Load the container with the alphabet letters after first scrolling 
+    window.addEventListener('scroll', function(e) {         
+        if( isOnScreen($('.grossary-alphabet'))) {
+            $('.grossary-alphabet').addClass('is-visible')
+        }
+    })
+});
+
